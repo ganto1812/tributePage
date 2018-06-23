58934ef73a156a30f19288f5c372136871cba3dc
ganto1812 <ganto1812@gmail.com>
2018-06-23 23:43:33 +0100

    initial commit

---
+++ tributePage.js
@@ -0,0 +1,13 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "100px";
+
+
+
+function liked()
+{   
+document.getElementById("like").innerHTML = "liked";
+  document.getElementById("like").style.color = "#adc2eb";
+  alert("THANK YOU FOR THE LIKE :)");
+}
+
+footer.style.fontSize = "12px";
+footer.style.textAlign = "right";
